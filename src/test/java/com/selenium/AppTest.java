be50ed696a3aae0b453cb85b0f8e801e27f4e77b
siddhi2709 <siddhi.palesh09@gmail.com>
2025-04-14 14:42:32 +0530

    Add SeleniumTestJob project files

---
+++ src/test/java/com/selenium/AppTest.java
@@ -0,0 +1,9 @@
+package com.selenium;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+public class AppTest {
+@Test
+public void testExample() {
+Assert.assertTrue(true, "This test case should always pass!");
+}
+}
